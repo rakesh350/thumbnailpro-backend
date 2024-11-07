dd1355f25524e1e6be323dd61493b437feb7020d
Rakesh Mishra <jrakesh173@gmail.com>
2024-11-06 23:44:04 -0800

    backend is able to read read and insert milestone text data.

--- src/routes/milstone.ts
+++ src/routes/milstone.ts
@@ -1,3 +1,5 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { verify } from "hono/jwt";
 
@@ -11,23 +13,59 @@ export const milestoneRouter = new Hono<{
     }
 }>();
 
+interface MilestoneType {
+    videoId: number,
+    viewThreshold: number,
+    thumbnailUrl: string
+}
+
 milestoneRouter.all('/*', async (c, next) => {
-    try{
+    try {
         const authHeader = c.req.header('Authorization');
         const token = authHeader?.replace('Bearer ', '');
-        if(!token) throw  new Error('Unauthorized. Token is missing');    
+        if (!token) throw new Error('Unauthorized. Token is missing');
         const decoded = await verify(token, c.env.JWT_SECRET);
-        if(!decoded) throw new Error('Unauthorized. Invalid token');
+        if (!decoded) throw new Error('Unauthorized. Invalid token');
         const userId = decoded.id || '';
-        c.set('userId',  userId.toString());
+        c.set('userId', userId.toString());
         await next();
-    } catch(error){
+    } catch (error: any) {
         c.status(401);
-        return  c.json({ error: error });
+        return c.json({ error: error.message });
     }
 });
 
-milestoneRouter.post('/save', (c) => {
-    const { videoId, milestones } = c.req.
-    return c.json({message: 'User validated'});
-})
+milestoneRouter.post('/save', async (c) => {
+    const body = await c.req.parseBody();
+    const videoId = body['videoId'] || '';
+    const milestoneArr: MilestoneType[] = [];
+
+    try {
+        const prisma = new PrismaClient({ datasourceUrl: c.env.ACC_DATABASE_URL }).$extends(withAccelerate());
+        const video = await prisma.video.findUnique({ where: { videoId: videoId.toString() } });
+
+        if (!video) {
+            c.status(422);
+            return c.json({ msg: 'Video not found' });
+        }
+        Object.keys(body).forEach((key) => {
+            if (key !== 'videoId') {
+                milestoneArr.push({
+                    videoId: video?.id,
+                    viewThreshold: Number(key),
+                    thumbnailUrl: 'thumbnail.png'
+                });
+            }
+        });
+
+        const milestones = await prisma.milestone.createMany({
+            data: milestoneArr,
+            skipDuplicates: true,
+        });
+
+        return  c.json({ msg: 'Milestone saved successfully', milestones });
+    } catch (error) {
+        c.status(422);
+        return c.json({ msg: 'Invalid video' });
+    }
+});
